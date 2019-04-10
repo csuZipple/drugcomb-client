275719756ed5ef007a3061359c752f5c37647a47
csuZipple <csu_zipple@qq.com>
2019-04-10 18:50:30 +0800

    feat: 添加表格组件

--- src/components/Table/SimpleTable.vue
+++ src/components/Table/SimpleTable.vue
@@ -1,13 +1,117 @@
 <template>
-    $END$
+  <table>
+    <tr>
+      <th v-for="(item, index) in header" :key="index">
+        <div class="title-container" @click="sortData(item, typeof body[0][item] === 'number')">
+          <span>{{item}}</span>
+          <img v-if="typeof body[0][item] === 'number'" src="../../assets/icon/sort.png"  alt="">
+        </div>
+      </th>
+    </tr>
+    <tr v-for="(row, index) in body" :key="index">
+      <td :class="{'link': headerLinkKeys.includes(key)}" v-for="(col, key) in row" :key="key+ 'a' +index" @click="handleItemClick(col, headerLinkKeys.includes(key))">{{col}}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
-    export default {
-        name: "SimpleTable"
+export default {
+  name: 'SimpleTable',
+  props: {
+    header: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    linkIndexList: {
+      type: Array,
+      default () {
+        return [4, 5]
+      }
+    },
+    body: {
+      type: Array,
+      default () {
+        return []
+      }
     }
+  },
+  methods: {
+    handleItemClick (col, flag) {
+      if (flag) {
+        this.$emit('itemClicked', col)
+      }
+    },
+    sortData (key, flag) {
+      if (flag) {
+        if (this.body[0][key] <= this.body[1][key]) {
+          this.$emit('sort', this.body.sort((pre, next) => {
+            return next[key] - pre[key]
+          }))
+        } else {
+          this.$emit('sort', this.body.sort((pre, next) => {
+            return pre[key] - next[key]
+          }))
+        }
+      }
+    }
+  },
+  computed: {
+    headerLinkKeys () {
+      return this.header.filter((item, index) => this.linkIndexList.includes(index))
+    }
+  }
+}
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+  table{
+    font-family: Consolas,Menlo,Courier,monospace;
+    font-size: 12px;
+    border-collapse: collapse;
+    border-spacing: 0;
+    empty-cells: show;
+    border: 1px solid #e9e9e9;
+    width: 100%;
+    margin-bottom: 24px;
+    tr,td{
+      transition: all 0.3s;
+    }
+    tr:hover{
+      background: #f7f7f7;
+      &>td{
+        border-color: #f7f7f7;
+      }
+    }
+    th{
+      background: #f7f7f7;
+      white-space: nowrap;
+      color: #5c6b77;
+      font-weight: 600;
+      cursor: default;
+    }
+    th,td{
+      border: 1px solid #e9e9e9;
+      padding: 8px 16px;
+      text-align: left;
+    }
+
+    .link{
+      color: #2d8cf0;
+      cursor: default;
+      &:hover{
+        text-decoration: underline;
+      }
+    }
+
+    .title-container{
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 
 </style>
--- src/views/Test/Test1.vue
+++ src/views/Test/Test1.vue
@@ -10,17 +10,20 @@
     </p>
     <router-link to="/test2">GO Test 2</router-link>
 
+    <SimpleTable :header="header" :body="body" @itemClicked="handleItemClick" @sort="handleSort"/>
     <Page @pageClick="handleChangePage" :total="total" :current="current" :page-size="pageSize" @pageSizeChange="handlePageSizeChange"></Page>
+
   </div>
 </template>
 
 <script>
 import {getOriginName, getDrugCombPagination} from '../../api/api'
 import Page from '../../components/Paging/Paging'
+import SimpleTable from '../../components/Table/SimpleTable'
 
 export default {
   name: 'Test1',
-  components: {Page},
+  components: {SimpleTable, Page},
   methods: {
     fetchData () {
       getOriginName(5).then(data => {
@@ -35,16 +38,75 @@ export default {
     handleChangePage (index) {
       this.current = index
     },
-    handlePageSizeChange (size) {
-      this.current = Math.ceil(Math.ceil(this.total / size) * this.current / Math.ceil(this.total / this.pageSize))
+    handlePageSizeChange (current, size) {
+      this.current = current
       this.pageSize = size
+    },
+    handleSort (newBody) {
+      this.body = newBody
+    },
+    handleItemClick (item) {
+      console.log(item)
     }
   },
   data () {
     return {
       current: 1,
       total: 100,
-      pageSize: 10
+      pageSize: 10,
+      body: [{
+        'id': 1,
+        'blockId': 1,
+        'row': 1,
+        'col': 1,
+        'drugRowName': 'Thioguanine',
+        'drugColName': 'Chlorambucil',
+        'concRow': 0,
+        'concCol': 0,
+        'concRowUnit': 'uM',
+        'concColUnit': 'uM',
+        'response': 100
+      },
+      {
+        'id': 2,
+        'blockId': 1,
+        'row': 1,
+        'col': 2,
+        'drugRowName': 'Thioguanine',
+        'drugColName': 'Chlorambucil',
+        'concRow': 0,
+        'concCol': 1,
+        'concRowUnit': 'uM',
+        'concColUnit': 'uM',
+        'response': 101.37
+      },
+      {
+        'id': 3,
+        'blockId': 1,
+        'row': 1,
+        'col': 3,
+        'drugRowName': 'Thioguanine',
+        'drugColName': 'Chlorambucil',
+        'concRow': 0,
+        'concCol': 10,
+        'concRowUnit': 'uM',
+        'concColUnit': 'uM',
+        'response': 94.95
+      },
+      {
+        'id': 4,
+        'blockId': 1,
+        'row': 1,
+        'col': 4,
+        'drugRowName': 'Thioguanine',
+        'drugColName': 'Chlorambucil',
+        'concRow': 0,
+        'concCol': 100,
+        'concRowUnit': 'uM',
+        'concColUnit': 'uM',
+        'response': 27.143
+      }],
+      header: ['id', 'blockId', 'row', 'col', 'drugRowName', 'drugColName', 'concRow', 'concCol', 'concRowUnit', 'concColUnit', 'response']
     }
   }
 }
